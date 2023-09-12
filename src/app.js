54129ac051b4d2dae5b1618ac174209490bd46b8
Mateo Carmelo <analistaprogramador.b@corpacam.com.gt>
2023-09-11 23:20:41 -0600

    Update

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const http = require("http");
 const env = require("dotenv").config();
 const cors = require("cors");
-const { connectToMongoDB } = require("../src/Config/database");
+const {
+  connectToMongoDB
+} = require("../src/Config/database");
 
 //TODO: TODO ESTO ES PARA RUTAS NORMAL
 // const routes = require("./Routes/numerosChuj");
@@ -16,28 +19,32 @@ const httpServer = http.createServer(app);
 const port = process.env.PORT || 3434;
 
 app.use(express.json());
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 app.use("/api", routes);
 
 app.use("/Storage", express.static("Storage"));
 
 app.get("/", (req, res) => {
-  res.json({ message: "Bienvenidos" });
+  res.json({
+    message: "Bienvenidos"
+  });
 });
 
-/*mongoose
-.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUniFiedTopology: true,
-})
-.then((data) => {
- const url = `conetado a MONGODB ${data.connection.host} - en puerto ${data.connection.port}`;
-   console.log(url);
-  console.log("Conexion exitosa");
-})
-.catch((err) => console.log("Error al conectar a MongoDB", err));*/
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUniFiedTopology: true,
+  })
+  .then((data) => {
+    const url = `conetado a MONGODB ${data.connection.host} - en puerto ${data.connection.port}`;
+    console.log(url);
+    // console.log("Conexion exitosa");
+  })
+  .catch((err) => console.log("Error al conectar a MongoDB", err));
 
-connectToMongoDB().then();
+// connectToMongoDB().then();
 
 httpServer.listen(port, () => {
   console.log("Aplicación corriendo en el puerto:", `http://localhost:${port}`);
